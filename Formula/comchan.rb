1ad0b98cdae2a613f6a0a074b86988bb430ee434
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-26 12:21:13 +0530

    Updated version of comchan to 0.2.1

--- Formula/comchan.rb
+++ Formula/comchan.rb
@@ -1,10 +1,10 @@
 class Comchan < Formula
   desc "A Blazingly Fast Serial monitor written in Rust with Plotting support"
   homepage "https://github.com/Vaishnav-Sabari-Girish/ComChan"
-  url "https://github.com/Vaishnav-Sabari-Girish/ComChan/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "cff3ee5fb05063abe1534689a7a32bebaf50a8982007b033c81abf9a7acd4505"
+  url "https://github.com/Vaishnav-Sabari-Girish/ComChan/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "5b070515b81ab1325ccea6e042c3d4c9e1d3d1111c05bf41554ec60959120a95"
   license "MIT"   
-  version "0.2.0"
+  version "0.2.1"
 
   depends_on "rust" => :build
 
