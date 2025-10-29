d8bd596ae24cddad51efc1970f3bd05837807b95
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-29 15:06:44 +0530

    Updated sbor version to 0.1.8

--- Formula/sbor.rb
+++ Formula/sbor.rb
@@ -1,10 +1,10 @@
 class Sbor < Formula
   desc "C Project Manager"
   homepage "https://github.com/Vaishnav-Sabari-Girish/sbor"
-  url "https://github.com/Vaishnav-Sabari-Girish/sbor/archive/v0.1.7.tar.gz"
-  sha256 "fdbe960c179ec195b559dc7c0c7aaaf033826cd327c3818481c9a00fd9418b11"
+  url "https://github.com/Vaishnav-Sabari-Girish/sbor/archive/v0.1.8.tar.gz"
+  sha256 "eaabb494f2901b0d867f123f78024a132f14afe9bde06864896e1395b766e56c"
   license "MIT"
-  version "0.1.7"
+  version "0.1.8"
 
   depends_on "cmake" => :build
 
