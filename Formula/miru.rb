25c21b37854eac425344074ab95aad1c7a989609
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-08-05 09:00:54 +0530

    chore(miru): new miru version
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- Formula/miru.rb
+++ Formula/miru.rb
@@ -1,10 +1,10 @@
 class Miru < Formula
   desc "A zooming daemon for wayland"
   homepage "https://github.com/Vaishnav-Sabari-Girish/miru"
-  url "https://github.com/Vaishnav-Sabari-Girish/miru/releases/download/v0.3.0/miru-v0.3.0-linux-x86_64.tar.gz"
-  sha256 "88675e9c0227e47140ebbba7ddb932c7d2373f4fdecd280c44bcf6da178a7440"
+  url "https://github.com/Vaishnav-Sabari-Girish/miru/releases/download/v0.4.0/miru-v0.4.0-linux-x86_64.tar.gz"
+  sha256 "bf034bcc07bab6cbdadb7a2b7c490f03e2e6a4675babf807970c335138fbb488"
   license "MIT"   
-  version "0.3.0"
+  version "0.4.0"
 
   depends_on :linux 
 
