a35b2e384ccf59f6e5a36db848d005cdd837479f
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-08-15 18:56:43 +0530

    fix(sha256) : sha256 of miru updated
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- Formula/miru.rb
+++ Formula/miru.rb
@@ -2,7 +2,7 @@ class Miru < Formula
   desc "A zooming daemon for wayland"
   homepage "https://codeberg.org/Vaishnav-Sabari-Girish/miru"
   url "https://codeberg.org/Vaishnav-Sabari-Girish/miru/releases/download/v0.5.0/miru-v0.5.0-linux-x86_64.tar.gz"
-  sha256 "216b1dad7b109af70a44bcc8c3ad0050751c570c0b965623016f345f592a4d18"
+  sha256 "fde9dc19036b9fdf9a0f844c3548cd5bca2889999cb37b058674cda2999bfc96"
   license "MIT"   
   version "0.5.0"
 
