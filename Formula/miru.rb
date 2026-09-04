a9aabd10241861367885caf93f971b3c77950eac
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-09-04 09:30:45 +0530

    feat(miru): nump `miru` version
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- Formula/miru.rb
+++ Formula/miru.rb
@@ -1,10 +1,10 @@
 class Miru < Formula
   desc "A zooming daemon for wayland"
   homepage "https://codeberg.org/Vaishnav-Sabari-Girish/miru"
-  url "https://codeberg.org/Vaishnav-Sabari-Girish/miru/releases/download/v0.7.0/miru-v0.7.0-linux-x86_64.tar.gz"
+  url "https://codeberg.org/Vaishnav-Sabari-Girish/miru/releases/download/v0.8.0/miru-v0.8.0-linux-x86_64.tar.gz"
   sha256 "c7505096059614ca06984aa288cb1169c6505ba73c3d8bbe4ba1aa336520da1e"
   license "MIT"   
-  version "0.7.0"
+  version "0.8.0"
 
   depends_on :linux 
 
