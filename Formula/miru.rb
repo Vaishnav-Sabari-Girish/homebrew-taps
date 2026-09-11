39dfbe31dda5d48f4522463d7d121c0780adb40e
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-09-11 16:57:31 +0530

    feat(miru): update miru to 0.9.0
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- Formula/miru.rb
+++ Formula/miru.rb
@@ -1,10 +1,10 @@
 class Miru < Formula
   desc "A zooming daemon for wayland"
   homepage "https://codeberg.org/Vaishnav-Sabari-Girish/miru"
-  url "https://codeberg.org/Vaishnav-Sabari-Girish/miru/releases/download/v0.8.0/miru-v0.8.0-linux-x86_64.tar.gz"
-  sha256 "c7505096059614ca06984aa288cb1169c6505ba73c3d8bbe4ba1aa336520da1e"
+  url "https://codeberg.org/Vaishnav-Sabari-Girish/miru/releases/download/v0.9.0/miru-v0.9.0-linux-x86_64.tar.gz"
+  sha256 "a46fd11803e9b9e4b9e40563bdd072ab77f83364b1c5f86cc2c5a029456b2d01"
   license "MIT"   
-  version "0.8.0"
+  version "0.9.0"
 
   depends_on :linux 
 
